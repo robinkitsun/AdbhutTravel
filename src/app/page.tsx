7f7b7ed1f7ed0472f19b6dfa77ddad38fc275328
ANKIT SUNDRIYAL <ankitsundriyal0@gmail.com>
2025-06-25 17:56:47 +0000

    worked! use these link for others : Leh : https://www.adbhuttravel.com/w

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ const domesticPackages: Package[] = [
   {
     id: '2',
     name: '4 Person Leh Ladakh',
-    image: 'https://placehold.co/600x400.png',
+    image: 'https://www.adbhuttravel.com/wp-content/uploads/elementor/thumbs/2f3139a7-93e8-4446-ab1e-49abfc63c708-qbf1rs0z1iunz4nx07wz6oj7hbm3yhhcoy8b0gdm0o.jpg',
     dataAiHint: 'Ladakh mountains snow',
     duration: '04 Nights / 05 Days',
     packageType: '(4 Person)',
@@ -28,7 +28,7 @@ const domesticPackages: Package[] = [
   {
     id: '3',
     name: 'Char dham yatra',
-    image: 'https://placehold.co/600x400.png',
+    image: 'https://www.adbhuttravel.com/wp-content/uploads/elementor/thumbs/tourists-taking-photos-beautiful-scenery-skiing-around-deogyusan-qbf1rtwnf6x8mcl6p8q8bo24o3cudvotd7j9z0ato8.jpg',
     dataAiHint: 'himalayas temple pilgrimage',
     duration: '04 Nights / 05 Days',
     packageType: '(Honeymoon Couple)',
@@ -40,7 +40,7 @@ const internationalPackages: Package[] = [
   {
     id: '4',
     name: 'Couple Malaysia trip',
-    image: 'https://placehold.co/600x400.png',
+    image: 'https://www.adbhuttravel.com/wp-content/uploads/elementor/thumbs/cityscape-singapore-city-skyline-qbf1ruuhm0yixyjtjr4uw5tl9h87lksjpc6rga9fi0.jpg',
     dataAiHint: 'Malaysia city skyline',
     duration: '04 Nights / 05 Days',
     packageType: '(Couple)',
@@ -49,7 +49,7 @@ const internationalPackages: Package[] = [
   {
     id: '5',
     name: 'Paris tour',
-    image: 'https://placehold.co/600x400.png',
+    image: 'https://www.adbhuttravel.com/wp-content/uploads/elementor/thumbs/famous-eiffel-tower-paris-with-gorgeous-colors-qbf1rs0z1iunz4nx07wz6oj7hbm3yhhcoy8b0gdm0o.jpg',
     dataAiHint: 'Eiffel Tower',
     duration: '04 Nights / 05 Days',
     packageType: '(One Person)',
@@ -58,7 +58,7 @@ const internationalPackages: Package[] = [
   {
     id: '6',
     name: 'Honeymoon in Bali',
-    image: 'https://placehold.co/600x400.png',
+    image: 'https://www.adbhuttravel.com/wp-content/uploads/elementor/thumbs/bali-pagoda-indonesia-qbf1rvsbsuzt9kige9jhgnl1uv3kt9wa1gu8xk81bs.jpg',
     dataAiHint: 'Bali temple lake',
     duration: '7 night 8 days',
     packageType: '(Honeymoon Couple)',
