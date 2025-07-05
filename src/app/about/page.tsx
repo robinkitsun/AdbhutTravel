d00b51229e31667bae64dc4956e0d6e03fecdf90
Robin Sharma <robin.sharma23102001@gmail.com>
2025-07-05 14:19:02 +0000

    for Aditi chaneg teh designation to Director Sales
    For Mohit change to

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
 };
 
 const teamMembers = [
-  { name: "Mohit Sharma", role: "Founder & CEO", image: "https://media.licdn.com/dms/image/v2/D4D03AQEby5crkpPERQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1719289726893?e=2147483647&v=beta&t=QtcfIgIbkwq1Jr2IB7BXeVfOr89PuLox_Y5crNwBcB0", dataAiHint: "man portrait" },
-  { name: "Aditi Sharma", role: "Sales Head", image: "", dataAiHint: "woman portrait" },
+  { name: "Mohit Sharma", role: "Founder & Managing Director", image: "https://media.licdn.com/dms/image/v2/D4D03AQEby5crkpPERQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1719289726893?e=2147483647&v=beta&t=QtcfIgIbkwq1Jr2IB7BXeVfOr89PuLox_Y5crNwBcB0", dataAiHint: "man portrait" },
+  { name: "Aditi Sharma", role: "Director Sales", image: "", dataAiHint: "woman portrait" },
   { name: "Sachin", role: "Operation Manager", image: "", dataAiHint: "man portrait" },
 ];
 
